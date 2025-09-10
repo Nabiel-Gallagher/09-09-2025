5cc76aa46d29afae5a05e966021f375fd157338c
Nabiel Gallagher <deniza_fatih_34rpl@student.smktelkom-mlg.sch.id>
2025-09-10 07:20:48 +0700

    Update JumlahDeretBilangan.java

--- JumlahDeretBilangan.java
+++ JumlahDeretBilangan.java
@@ -2,14 +2,14 @@ import java.util.Scanner;
 
 public class JumlahDeretBilangan {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
         System.out.print("Masukkan Jumlah Siswa: ");
-        int Awal = scanner.nextInt();
+        int Awal = input.nextInt();
         int temp = 0;
 
         for (int i = 1; i <= Awal; i++) {
             System.out.print("Masukkan nilai siswa ke-" + i + ": ");
-            int nilai = scanner.nextInt();
+            int nilai = input.nextInt();
             temp += nilai;
         }
 
